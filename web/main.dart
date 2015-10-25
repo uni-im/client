cdfd9df3baf35bd87cabe80eead3a7339b6482d4
Benjamin Campbell <benji@benjica.com>
2015-10-25 16:03:24 -0600

    Add simple project scaffold

---
+++ web/main.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+
+import 'package:client/client.dart';
+
+void main() {
+  Awesome you = new Awesome();
+
+  querySelector('#output').text =
+      you.isAwesome ? 'You awesome!' : 'You could be awesome';
+}
