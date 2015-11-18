76695778380b9869c5e25d4b09c0fbb147d916b5
Benjamin Campbell <benji@benjica.com>
2015-11-18 13:40:51 -0700

    Add agent tests

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -1,11 +1,13 @@
 import 'package:test/test.dart';
 
+import 'agent_test.dart' as agent_tests;
 import 'messages/markdown_test.dart' as markdown_tests;
 import 'messages/message_test.dart' as message_tests;
 import 'transports/transport_client_test.dart' as transport_client_tests;
 import 'channel_test.dart' as channel_tests;
 
 void main() {
+  group('', agent_tests.main);
   group('', channel_tests.main);
   group('', markdown_tests.main);
   group('', message_tests.main);
