cb2032c0beb874da12ac48e563d1bc04ca982acb
Benjamin Campbell <benji@benjica.com>
2015-11-18 22:58:12 -0700

    Add loopback tests

---
+++ test/transports/loopback_client_test.dart
@@ -0,0 +1,23 @@
+library test.transports.loopback_client_test;
+
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:client/client.dart';
+
+import '../utils/mocks.dart';
+
+main() {
+  group('LoopbackTransportClient', () {
+    test('should notify a channel on submission', () async {
+      var client = new LoopbackTransportClient();
+      var channel = new MockChannel();
+      var message = new MockMessage();
+
+      client.join(channel);
+      await client.send(channel, message);
+
+      verify(channel.receive(message));
+    });
+  });
+}
