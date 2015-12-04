6a5864b145f5b3396483e359f6e8c6a527e25339
Benjamin Campbell <benji@benjica.com>
2015-12-03 22:55:21 -0700

    Seperate control message tests

--- test/messages/control_message.dart
+++ test/messages/control_message.dart
@@ -1,11 +1,11 @@
 library test.messages.control_message_test;
 
-import 'package:test/test.dart';
-import 'package:mockito/mockito.dart';
-
 import 'package:client/src/messages/message.dart';
-import '../utils/mocks.dart';
 import 'package:client/src/messages/presenter.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import '../utils/mocks.dart';
 
 void main() {
   group('ControlMessage', () {
@@ -17,12 +17,17 @@ void main() {
       var mockPresenterFactory = new MockPresenterFactory();
 
       when(mockPresenterFactory.getPresenter(any)).thenReturn(presenter);
+      when(presenter.present()).thenReturn('test message');
       messageFactory = new MessageFactory(mockPresenterFactory);
     });
 
     test('should marshal to empty map', () {
       var message = messageFactory.controlMessage('test message');
       expect(message.marshal(), equals({}));
+    });
+
+    test('should render presenter output', () {
+      var message = messageFactory.controlMessage('test message');
       expect(message.render(), equals('test message'));
       verify(presenter.present()).called(1);
     });
