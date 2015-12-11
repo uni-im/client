11f19fa3ffb6385280abcfc3c888b4a1940cc896
Benjamin Campbell <benji@benjica.com>
2015-12-10 22:02:03 -0700

    Refactored scraper scrape call

--- lib/src/client/url_scraper.dart
+++ lib/src/client/url_scraper.dart
@@ -3,22 +3,24 @@ import 'dart:async';
 import 'package:html/parser.dart';
 import 'package:html/dom.dart';
 
-class UrlScraper
-{
-  Future<String> getTitle(String url) async
-  {
+class UrlScraper {
+  Document _document;
+  HttpClient _client;
 
-    return _getHtml(url).then(
-      (document) => document.querySelector('title').text
-    );
+  UrlScraper({HttpClient client}) {
+    _client = client ?? new HttpClient();
   }
 
+  String get title => _document.querySelector('title').text;
+
   /// fetch and parse the HTML from [url]
-  Future<Document> _getHtml(String url) async => new HttpClient()
-  .getUrl(Uri.parse(url))
-  .then((req) => req.close())
-  .then((res) => res
-  .asyncExpand((bytes) => new Stream.fromIterable(bytes))
-  .toList())
-  .then((bytes) => parse(bytes, sourceUrl: url));
+  Future scrape(String url) async {
+    var req = await _client.getUrl(Uri.parse(url));
+    HttpClientResponse response = await req.close();
+    var bytes = await response
+        .asyncExpand((bytes) => new Stream.fromIterable(bytes))
+        .toList();
+
+    _document = await parse(bytes, sourceUrl: url);
+  }
 }
--- test/url_scraper_test.dart
+++ test/url_scraper_test.dart
@@ -1,22 +1,20 @@
+import 'dart:async';
+
 import 'package:test/test.dart';
 
 import 'package:client/src/client/url_scraper.dart';
 
-void main()
-{
-  group('URL Scraper', ()
-  {
+void main() {
+  group('URL Scraper', () {
     UrlScraper scraper;
 
-    setUp(()
-    {
+    setUp(() {
       scraper = new UrlScraper();
     });
 
-    test('should get title "Example Domain" for example.com', ()
-    {
-      String url = 'http://example.com';
-      scraper.getTitle(url).then(expectAsync((title) => expect(title, equals('Example Domain'))));
+    test('should get title "Example Domain" for example.com', () async {
+      await scraper.scrape('http://example.com');
+      expect(scraper.title, equals('Example Domain'));
     });
   });
 }
